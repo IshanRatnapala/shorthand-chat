16e62d76d5d9feb06916faaa14f9764b1c5c0b61
Dragfreeze <Dragfreeze@gmail.com>
2017-03-10 20:00:04 +0530

    added messaging functionality

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,30 @@
-global.jQuery = require('jquery');
+var $ = global.jQuery = require('jquery');
 var io = require('socket.io-client');
-var socket = io();
+var socket = global.socket = io();
 
+// UI elements
+var $form = $('#message-form');
+var $messages = $('#messages');
+var $input = $form.find('input');
+
+// Setup socket.io
+socket.on('connect', function (socket) {
+    console.log('Connected to socket.io server.');
+});
+socket.on('message', function (data) {
+    console.log('New message:', data.text);
+
+    $messages.append('<p>' + data.text + '</p>')
+});
+
+// Submit new message
+$form.on('submit', function (event) {
+    event.preventDefault();
+    var message = $input.val().trim();
+    if (message.length) {
+        socket.emit('message', {
+            text: message
+        });
+        $input.val('');
+    }
+});
--- server.js
+++ server.js
@@ -4,14 +4,26 @@ var socket = require('socket.io');
 var app = express();
 
 /* Start Express */
-var server = app.listen(PORT, function () {
-    console.log('Server started on port', PORT);
-});
+var server = require('http').Server(app);
+
 /* Start Socket */
-var io = socket.listen(server);
+var io = socket(server);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
     console.log('User connected via socket.io');
+
+    socket.on('message', function (message) {
+        console.log('Message received', message.text);
+        io.emit('message', message);
+    });
+
+    socket.emit('message', {
+        text: 'Welcome to Shorthand Chat'
+    });
+});
+
+server.listen(PORT, function () {
+    console.log('Server started on port', PORT);
 });
