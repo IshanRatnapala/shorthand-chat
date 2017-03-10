8e3635c6fedf1b73df83555f5a2007de05621f40
Dragfreeze <Dragfreeze@gmail.com>
2017-03-10 17:13:03 +0530

    project setup

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
+var socket = require('socket.io');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
+/* Start Express */
+var server = app.listen(PORT, function () {
+    console.log('Server started on port', PORT);
+});
+/* Start Socket */
+var io = socket.listen(server);
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function () {
     console.log('User connected via socket.io');
-});
-
-http.listen(PORT, function () {
-    console.log('Server started on port', PORT);
 });
